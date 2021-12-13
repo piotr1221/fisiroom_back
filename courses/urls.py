59a65e17dd59737d90ca217f8f663a9aa02f01e4
Edwin Aguilar <edwin.aguilar@unmsm.edu.pe>
2021-12-13 16:35:14 -0500

    adecuaciones de app de cursos

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,8 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from . import views
 
-router = DefaultRouter()
-router.register(r'owner', views.CourseOwnerViewSet, basename='courses_owner')
-router.register(r'student', views.CourseStudentViewSet, basename='courses_student')
+router = SimpleRouter()
+router.register(r'created', views.CourseCreatedViewSet, basename='created')
+router.register(r'enrolled', views.CourseEnrolledViewSet, basename='enrolled')
 
 urlpatterns = router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,23 +1,23 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 from . import serializers
 # Create your views here.
 
-class CourseOwnerViewSet(viewsets.ModelViewSet):
+class CourseCreatedViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CourseSerializer
-    queryset = serializer_class.Meta.model.objects.all()
-    '''def get_queryset(self, pk=None):
-        if pk is None:
-            return self.serializer_class.Meta.model.objects.filter(owner=self.req.user).all()
-        return self.serializer_class.Meta.model.objects.filter(id=pk, owner=self.req.user).first()'''
+    queryset = serializer_class.Meta.model.objects
 
-    def list(self, req, pk=None):
-        if pk:
-            queryset = self.serializer_class.Meta.model.objects.filter(id=pk, owner=self.req.user).first()
-        else:
-            queryset = self.serializer_class.Meta.model.objects.filter(owner=req.user)
+    def list(self, req):
+        queryset = self.serializer_class.Meta.model.objects.filter(owner=req.user)
         serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, req, pk=None):
+        queryset = self.serializer_class.Meta.model.objects.filter(owner=req.user)
+        course = get_object_or_404(queryset, pk=pk)
+        serializer = self.serializer_class(course)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, req):
@@ -25,9 +25,9 @@ class CourseOwnerViewSet(viewsets.ModelViewSet):
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         serializer.save()
-        return Response({'message': 'Curso creado con éxito'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class CourseStudentViewSet(viewsets.ModelViewSet):
+class CourseEnrolledViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CourseSerializer
 
     def get_queryset(self, pk=None):
