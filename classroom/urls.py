6bdd3310e950f3c42dfa4acf1cdc435c8af4f60c
Alexis Arias <alexis.arias555@gmail.com>
2022-01-06 22:42:42 -0500

    post endpoint

--- classroom/urls.py
+++ classroom/urls.py
@@ -6,6 +6,9 @@ from . import views
 main_router = SimpleRouter()
 main_router.register(r'', views.ClassroomCourseViewSet, basename='classroom')
 
+post_router = SimpleRouter()
+post_router.register(r'posts', views.ClassroomPostViewSet, basename='posts')
+
 assignment_router = SimpleRouter()
 assignment_router.register(r'assignments', views.ClassroomAssignmentViewSet, basename='assignments')
 
@@ -14,6 +17,7 @@ homework_router.register(r'submit', views.ClassroomHomeworkViewSet, basename='ho
 
 urlpatterns = [
     path('', include(main_router.urls)),
+    path('<int:course_id>/', include(post_router.urls)),
     path('<int:course_id>/', include(assignment_router.urls)),
     path('<int:course_id>/assignments/<int:assign_id>/', include(homework_router.urls))
 ]       
