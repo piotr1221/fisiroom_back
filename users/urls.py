d359e374062bedba9eebecbaadfec1d76bae48ca
Juan Diego Pérez Fonseca <juandiegoperezf@hotmail.com>
2022-01-11 23:56:57 -0500

    Agregar nota a usuario

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
 from rest_framework.routers import SimpleRouter
 from . import views
+from django.urls import path, include
 
-router = SimpleRouter()
-router.register(r'', views.UserViewSet, basename='users')
+main_router = SimpleRouter()
+main_router.register(r'', views.UserViewSet, basename='users')
 
-urlpatterns = router.urls
+grade_router = SimpleRouter()
+grade_router.register(r'', views.UserGradeViewSet, basename='grades')
+
+urlpatterns = [
+    path('', include(main_router.urls)),
+    path('grades/', include(grade_router.urls))   
+]    
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from users.models import User
@@ -37,3 +38,17 @@ class UserViewSet(viewsets.ModelViewSet):
 
         serializer = serializers.UserLoginSerializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserGradeViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserGradeSerializer
+    queryset = serializer_class.Meta.model.objects
+
+    def update(self, request, pk=None):
+        queryset = self.queryset.filter()
+        student = get_object_or_404(queryset, pk=pk)
+        serializer = self.serializer_class(student, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
