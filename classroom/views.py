7df79e369a2522e6f7bda798efadcd87bf419a8d
Edwin Aguilar <edwin.aguilar@unmsm.edu.pe>
2022-01-13 17:40:11 -0500

    ra

--- classroom/views.py
+++ classroom/views.py
@@ -74,7 +74,7 @@ class ClassroomCourseViewSet(viewsets.ModelViewSet):
 
     def create(self, req):
         serializer = self.serializer_class(data=req.data, context={'owner': req.user})
-        return Response(req.files.get('image'))
+        return Response(req.FILES.get('image'))
         # if not serializer.is_valid():
         #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         # serializer.save()
